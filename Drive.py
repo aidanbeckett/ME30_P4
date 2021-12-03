9ed9b47d4c92c001c6af910ffecce798ecf735aa
shawnk3 <shawnekuriakose@gmail.com>
2021-12-03 20:29:34 +0000

    robotcodetest1works

--- Drive.py
+++ Drive.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #define actuators GPIOs
@@ -16,14 +16,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(enA,GPIO.OUT) 
+GPIO.output(enA,GPIO.HIGH)
 	
 @app.route("/")
 def index():
   #stop
   GPIO.output(in1,GPIO.LOW)
   GPIO.output(in2,GPIO.LOW)
-  GPIO.output(in3,GPIO.LOW)
-  GPIO.output(in4,GPIO.LOW)
+  # GPIO.output(in3,GPIO.LOW)
+  # GPIO.output(in4,GPIO.LOW)
   print("stop")
 #	templateData = {
 #	 	'button'  : buttonSts,
@@ -33,7 +34,7 @@ def index():
 #      		'ledGrn'  : ledGrnSts,
 #	}
 #	return render_template('index.html', **templateData)
-return render_template('index.html')
+  return render_template('index.html')
 	
 @app.route("/<action>")
 def action(action):
@@ -41,23 +42,23 @@ def action(action):
   if action == "left":
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    GPIO.output(in3, GPIO.LOW)
-    GPIO.output(in4, GPIO.HIGH)
+    # GPIO.output(in3, GPIO.LOW)
+    # GPIO.output(in4, GPIO.HIGH)
   if action == "right":
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
-    GPIO.output(in3, GPIO.HIGH)
-    GPIO.output(in4, GPIO.LOW)
+    # GPIO.output(in3, GPIO.HIGH)
+    # GPIO.output(in4, GPIO.LOW)
   if action == "forward":
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    GPIO.output(in3, GPIO.HIGH)
-    GPIO.output(in4, GPIO.LOW)
+    # GPIO.output(in3, GPIO.HIGH)
+    # GPIO.output(in4, GPIO.LOW)
   if action == "backwards":
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
-    GPIO.output(in3, GPIO.LOW)
-    GPIO.output(in4, GPIO.HIGH) 
+    # GPIO.output(in3, GPIO.LOW)
+    # GPIO.output(in4, GPIO.HIGH) 
    
 #	templateData = {
 #	      	'button'  : buttonSts,
@@ -67,7 +68,7 @@ def action(action):
 #      		'ledGrn'  : ledGrnSts,
 #	}
 #	return render_template('index.html', **templateData)
-return render_trmplate('index.html')
+  return render_template('index.html')
 
 if __name__ == "__main__":
-   app.run(host='10.245.86.142', port=80, debug=True)
+   app.run(host='http://10.245.86.142:5000', port=80, debug=True)
