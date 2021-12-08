a283504d45d03bc07944e3d53c3dea60b085b59c
shawnk3 <shawnekuriakose@gmail.com>
2021-12-08 21:41:47 +0000

    working code 12/7

--- Drive.py
+++ Drive.py
@@ -8,23 +8,28 @@ GPIO.setwarnings(False)
 #define actuators GPIOs
 in1 = 8 
 in2 = 10
-in3 = 15
-in4 = 16
+in3 = 16
+in4 = 18
 enA = 12
+enB = 22
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
 GPIO.setup(enA,GPIO.OUT) 
 GPIO.output(enA,GPIO.HIGH)
+GPIO.setup(enB,GPIO.OUT) 
+GPIO.output(enB,GPIO.HIGH)
 	
 @app.route("/")
 def index():
   #stop
   GPIO.output(in1,GPIO.LOW)
   GPIO.output(in2,GPIO.LOW)
-  # GPIO.output(in3,GPIO.LOW)
-  # GPIO.output(in4,GPIO.LOW)
+  GPIO.output(in3,GPIO.LOW)
+  GPIO.output(in4,GPIO.LOW)
   print("stop")
 #	templateData = {
 #	 	'button'  : buttonSts,
@@ -40,25 +45,29 @@ def index():
 def action(action):
    
   if action == "left":
+    print("left")
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    # GPIO.output(in3, GPIO.LOW)
-    # GPIO.output(in4, GPIO.HIGH)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
   if action == "right":
+    print("right")
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
-    # GPIO.output(in3, GPIO.HIGH)
-    # GPIO.output(in4, GPIO.LOW)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
   if action == "forward":
+    print("forward")
     GPIO.output(in1, GPIO.HIGH)
-    GPIO.output(in2, GPIO.LOW)
-    # GPIO.output(in3, GPIO.HIGH)
-    # GPIO.output(in4, GPIO.LOW)
-  if action == "backwards":
-    GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
-    # GPIO.output(in3, GPIO.LOW)
-    # GPIO.output(in4, GPIO.HIGH) 
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+  if action == "backwards":
+    print("backward")
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.HIGH) 
    
 #	templateData = {
 #	      	'button'  : buttonSts,
