2a9c65f3e170e6243d4d4ca69bd40ee8881e3459
aidanbeckett <71097830+aidanbeckett@users.noreply.github.com>
2021-12-12 13:29:41 -0500

    Update Drive.py

--- Drive.py
+++ Drive.py
@@ -12,6 +12,10 @@ in3 = 16
 in4 = 18
 enA = 12
 enB = 22
+step1 = 35
+step2 = 36
+step3 = 37
+step4 = 38
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(in1,GPIO.OUT)
@@ -19,10 +23,43 @@ GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
 GPIO.setup(enA,GPIO.OUT) 
+
 GPIO.output(enA,GPIO.HIGH)
 GPIO.setup(enB,GPIO.OUT) 
 GPIO.output(enB,GPIO.HIGH)
-	
+
+delay = 0.0055
+motorState = "down"
+
+def setStep(w1, w2, w3, w4):
+  GPIO.output(step1, w1)
+  GPIO.output(step2, w2)
+  GPIO.output(step3, w3)
+  GPIO.output(step4, w4)
+
+def makeStepsUp(steps):
+  for i in range(steps):
+    setStep(1,0,1,0)
+    time.sleep(delay)
+    setStep(0,1,1,0)
+    time.sleep(delay)
+    setStep(0,1,0,1)
+    time.sleep(delay)
+    setStep(1,0,0,1)
+    time.sleep(delay)
+
+def makeStepsDown(steps):
+  for i in range(steps)
+    setStep(1,0,0,1)
+    time.sleep(delay)
+    setStep(0,1,0,1)
+    time.sleep(delay)
+    setStep(0,1,1,0)
+    time.sleep(delay)
+    setStep(1,0,1,0)
+    time.sleep(delay)
+
+
 @app.route("/")
 def index():
   #stop
@@ -68,7 +105,24 @@ def action(action):
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.HIGH)
     GPIO.output(in4, GPIO.HIGH) 
-   
+  if action == "down":
+    if motorState == "up":
+      makeStepsDown(200)
+    if motorState == "med":#Move the motor to the up postion based on its current location
+      makeStepsDown(100)
+    motorState = "down" #Set the motor state to down
+  if action == "med":
+    if motorState == "up":
+      makeStepsDown(100)
+    if motorState == "down": #Move the motor to the medium postion based on its current location
+      makeStepsUp(100)
+    motorState = "med" #Set the motor state to medium
+  if action == "up":
+    if motorState == "med":
+      makeStepsUp(100)
+    if motorState == "med": #Move the motor to the down postion based on its current location
+      makeStepsUp(200)
+    motorState = "up" #Set the motor state to up
 #	templateData = {
 #	      	'button'  : buttonSts,
 #      		'senPIR'  : senPIRSts,
